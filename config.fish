2fb2d4855569e3a839bd536db15562eb45841384
tenzinhl <70078689+tenzinhl@users.noreply.github.com>
2024-11-01 10:11:42 -0700

    Update config.fish

--- config.fish
+++ config.fish
@@ -7,3 +7,6 @@ if command -v fzf >/dev/null 2>&1
     fzf --fish | source
     bind --erase \cr
 end
+
+# Add iterm integration when in a Mac environment where iterm2 is installed
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
